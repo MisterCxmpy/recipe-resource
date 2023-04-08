df0929cea0f0123718873db474b97828e2928411
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-08 16:53:10 +0100

    landing page styling

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Section from '../Section/Section'
 import "./Landing.css"
 
 export default function Landing() {
   return (
     <div className="landing">
       <div className='container'>
-        
+        <div className='info'>
+          <h1>Welcome to Recipe Resource! Discover delicious recipes from around the world and unleash your inner chef.</h1>
+          <p>Join us in the kitchen. Let's cook up something amazing!</p>
+          <button>Get Started!</button>
+        </div>
+        <div class='img background-img'></div>
+      </div>
+      <div className="sections">
+        <Section heading={"Discover More About Recipe Resource"}/>
       </div>
     </div>
   )
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Section({heading}) {
+  return (
+    <div className='section'>
+      <h1 className="heading">{heading}</h1>
+    </div>
+  )
+}
