76620b5d5de7cdaa6edc1c68af1899cb891f915a
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-08 15:47:09 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './layout/Nav/Nav'
+import Landing from './components/Landing/Landing'
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Landing />
+    </>
+  )
+}
---
+++ src/layout/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+  return (
+    <nav>
+      <div>  
+        <h1 className='title'>Recipe Resource</h1>
+        <div className='options'>
+          <button className='option btn'>Option 1</button>
+          <button className='option btn'>Option 2</button>
+          <button className='option btn'>Option 3</button>
+        </div>
+      </div>
+      <button className='login btn'>Login</button>
+    </nav>
+  )
+}
